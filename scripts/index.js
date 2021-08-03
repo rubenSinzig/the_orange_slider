31e711ea25987247c8fdf571be601d4aa6aa23ac
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-03 14:51:08 +0200

    html, css & js -> change all to make it work

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
+document
+  .querySelector(".compare #compare-ip")
+  .addEventListener("input", function (e) {
+    document.querySelector(".compare .before .after").style.width =
+      +e.target.value + "px";
+  });
+
+document
+  .querySelector(".compare #compare-ip")
+  .addEventListener("change", function (e) {
+    document.querySelector(".compare .before .after").style.width =
+      +e.target.value + "px";
+  });
+
 const getYear = document.querySelector(".year");
 const year = new Date().getFullYear();
 getYear.innerHTML = year;
